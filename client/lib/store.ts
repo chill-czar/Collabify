53461130d4b60ca5fcd715d5bdc14d6d6dfa7a1d
chill-czar <aisarthak2@gmail.com>
2025-08-16 18:16:58 +0530

    raw

--- client/lib/store.ts
+++ client/lib/store.ts
@@ -1,9 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import  userReducer  from '@/lib/slices/userSlice';
 // Import your reducers here
 
 export const store = configureStore({
   reducer: {
-
+    user: userReducer
     },
 //   devTools: process.env.NODE_ENV !== "production",
 });
